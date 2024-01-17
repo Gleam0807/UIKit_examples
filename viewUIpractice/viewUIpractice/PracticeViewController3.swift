f59426c3adebc378c1400047d8df00d74a029e2e
gleam0807 <teemocscsg@naver.com>
2024-01-17 20:24:16 +0900

    뷰연습2

---
+++ viewUIpractice/viewUIpractice/PracticeViewController3.swift
@@ -0,0 +1,14 @@
+//
+//  PracticeViewController3.swift
+//  viewUIpractice
+//
+//  Created by SUNG on 1/17/24.
+//
+
+import UIKit
+
+class PracticeViewController3: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

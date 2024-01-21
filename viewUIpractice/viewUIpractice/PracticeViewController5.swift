02916ceda10cf73e92a159030fdeb7cb7bee82e9
gleam0807 <teemocscsg@naver.com>
2024-01-21 18:05:22 +0900

    뷰연습4

---
+++ viewUIpractice/viewUIpractice/PracticeViewController5.swift
@@ -0,0 +1,14 @@
+//
+//  PracticeViewController5.swift
+//  viewUIpractice
+//
+//  Created by SUNG on 1/21/24.
+//
+
+import UIKit
+
+class PracticeViewController5: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
